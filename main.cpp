a8b6d56248ce7b223a5592312bf77c5c4f0e6dd1
korreajc <correajc1799@gmail.com>
2020-04-26 06:43:39 +0000

    Finished, maybe just add input validation tomorrow

--- main.cpp
+++ main.cpp
@@ -7,6 +7,8 @@ using namespace std;
 
 string sentence;
 
+string swapString(string);
+
 string getString();
 
 string spaceString(string);
@@ -19,10 +21,11 @@ int main() {
 
   string spacedString = spaceString(firstString);
 
-  string pigLatinString = pigLatin(spacedString);
+  string swapLettersString = swapString(spacedString);
+
+  string pigLatinString = pigLatin(swapLettersString);
 
   cout << pigLatinString << endl;
-  cout << pigLatinString.length();
 }
 
 string getString(){
@@ -45,32 +48,102 @@ string spaceString(string sentence){
     }
 
   }
-  cout << "Length" << sentence.length() << endl;
-  cout << "Spaced String: " << sentence << endl;
-
   return sentence;
 }
 
 string pigLatin(string spacedSentence){
 
-int i = 0;
-int zeroLocation;
-bool foundZero = false;
-string phrase = "ay";
+  int i = 0;
+  int zeroLocation;
+  bool foundZero = false;
+  string phrase = "ay";
 
-for( i = 0; i < spacedSentence.length(); i++){
+  for( i = 0; i < spacedSentence.length(); i++){
 
     if(spacedSentence[i] == ' '){
 
-      cout << "Zero Located at: " <<  i << endl;
       zeroLocation = i;
       spacedSentence.insert(zeroLocation, phrase);
-      cout << spacedSentence << endl;
       i = zeroLocation + 2;
     }  
-}
+  }
 
   spacedSentence.insert(spacedSentence.length(), phrase);
 
   return spacedSentence;
+}
+
+string swapString(string spacedSentence){
+
+  int zeroCounter = 0;
+  int zeroLocation;
+  int newFirst;
+
+  for(int i = 0; i < spacedSentence.length(); i++){   
+    
+    if(zeroCounter == 0 && spacedSentence[i] == ' '){
+
+      //very first  char
+      zeroLocation = i;
+      //first letter
+      char firstWordTempChar;
+      char firstWordLastTempChar;
+      string firstWordTempString;
+      string firstWordLastTempString;
+      firstWordTempChar = spacedSentence[0];
+      firstWordTempString.push_back(firstWordTempChar);
+      //last letter
+      firstWordLastTempChar = spacedSentence[zeroLocation-1];
+      firstWordLastTempString.push_back(firstWordLastTempChar);
+
+      //first letter turns to last letter
+      //last letter turns to first letter
+      spacedSentence.replace(zeroLocation, 1, firstWordTempString);
+      spacedSentence.erase(0,1);
+
+      spacedSentence.insert(zeroLocation, " ");
+      zeroCounter++;
+      i = zeroLocation+1;
+      newFirst = zeroLocation + 1;
+
+    }
+    else if(zeroCounter > 0 && spacedSentence[i] == ' ')
+    {
+      zeroLocation = i;
+      char tempOneO = spacedSentence[newFirst];
+      string tempNumOneS;
+      tempNumOneS.push_back(tempOneO);
+
+      char tempTwoO = spacedSentence[zeroLocation-1];
+      string tempNumTwoO;
+      tempNumTwoO.push_back(tempTwoO);
+
+      spacedSentence.replace(zeroLocation, 1, tempNumOneS);
+      spacedSentence.erase(newFirst, 1);
+      spacedSentence.insert(zeroLocation, " ");
+
+      zeroCounter++;
+      i = zeroLocation + 1;
+      newFirst = zeroLocation +1;    
+ 
+    }else if(i == spacedSentence.length()-1){
+
+      zeroLocation = newFirst - 1;
+
+      char firstLetterChar = spacedSentence[newFirst];
+      string firstLetterString;
+      firstLetterString.push_back(firstLetterChar);
+
+      char lastLetterChar = spacedSentence[i];
+      string lastLetterString;
+      lastLetterString.push_back(lastLetterChar);
+
+      spacedSentence.insert(i+1, " ");
+      spacedSentence.replace(i+1, 1, firstLetterString);
+      spacedSentence.erase(newFirst, 1);
+
+    }
+  }
+    return spacedSentence;
+
 }
